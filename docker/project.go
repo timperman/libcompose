df20fac9b759f95d2203410e0689667e018181c3
timperman <brett.timperman@gmail.com>
2016-07-18 17:10:56 -0400

    Update project.go

--- docker/project.go
+++ docker/project.go
@@ -21,7 +21,7 @@ import (
 const ComposeVersion = "1.5.0"
 
 // NewProject creates a Project with the specified context.
-func NewProject(context *Context, parseOptions *config.ParseOptions) (project.APIProject, error) {
+func NewProject(context *Context, parseOptions *config.ParseOptions) (*project.Project, error) {
 	if context.ResourceLookup == nil {
 		context.ResourceLookup = &lookup.FileConfigLookup{}
 	}
